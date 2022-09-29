231e41b8d2b4e42b14db0c8dff6818b783a5f7e6
Fluke <hunterx52110@gmail.com>
2022-09-29 23:28:02 +0700

    Completed

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -8,31 +8,57 @@ import { checkToken } from "../../../../backendLibs/checkToken";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     //check token
-
+    const username = checkToken(req);
+    if (!username)
+      return res.status(401).json({
+        ok: false,
+        message: "You don't have permission to access this api",
+      });
     //get roomId from url
     const roomId = req.query.roomId;
 
     const rooms = readChatRoomsDB();
-
+    const roomsIdx = rooms.find((x) => x.roomId === roomId);
+    if (roomsIdx === undefined) {
+      res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      res.status(200).json({ ok: true, messages: roomsIdx.messages });
+    }
     //check if roomId exist
 
     //find room and return
     //...
   } else if (req.method === "POST") {
     //check token
-
+    const username = checkToken(req);
+    if (!username)
+      return res.status(401).json({
+        ok: false,
+        message: "You don't have permission to access this api",
+      });
     //get roomId from url
     const roomId = req.query.roomId;
     const rooms = readChatRoomsDB();
-
+    const text = req.body.text;
+    const roomsIdx = rooms.find((x) => x.roomId === roomId);
     //check if roomId exist
-
-    //validate body
+    const newId = uuidv4();
+    const newMeg = {
+      messageId: newId,
+      text: text,
+      username: username.username,
+    };
+    if (roomsIdx === undefined) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
     if (typeof req.body.text !== "string" || req.body.text.length === 0)
       return res.status(400).json({ ok: false, message: "Invalid text input" });
-
-    //create message
-
-    writeChatRoomsDB(rooms);
+    else {
+      roomsIdx.messages.push(newMeg);
+      writeChatRoomsDB(rooms);
+      return res.status(200).json({ ok: true, message: newMeg });
+    }
   }
+  //validate body
+  //create message
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -8,15 +8,52 @@ export default function roomIdMessageIdRoute(req, res) {
   //get ids from url
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
-
   //check token
-
+  const username = checkToken(req);
+  if (!username)
+    return res.status(401).json({
+      ok: false,
+      message: "You don't have permission to access this api",
+    });
   const rooms = readChatRoomsDB();
+  const foundRoom = rooms.find((x) => x.roomId === roomId);
 
   //check if roomId exist
 
+  if (foundRoom === undefined) {
+    return res.status(404).json({ ok: false, message: "Invalid room id" });
+  }
   //check if messageId exist
+  const foundMessage = foundRoom.messages.find(
+    (x) => x.messageId === messageId
+  );
+  console.log(foundMessage);
+  if (foundMessage === undefined) {
+    return res.status(404).json({ ok: false, message: "Invalid message id" });
+  }
 
   //check if token owner is admin, they can delete any message
+  if (username.isAdmin) {
+    foundRoom.messages = foundRoom.messages.filter(
+      (x) => x.messageId !== messageId
+    );
+    rooms.messages = foundRoom.messages;
+    writeChatRoomsDB(rooms);
+    return res.json({ ok: true });
+  } else {
+    if (username.username === foundMessage.username) {
+      foundRoom.messages = foundRoom.messages.filter(
+        (x) => x.messageId !== messageId
+      );
+      rooms.messages = foundRoom.messages;
+      writeChatRoomsDB(rooms);
+      return res.json({ ok: true });
+    } else {
+      return res.status(401).json({
+        ok: false,
+        message: "You don't have permission to access this api",
+      });
+    }
+  }
   //or if token owner is normal user, they can only delete their own message!
 }
